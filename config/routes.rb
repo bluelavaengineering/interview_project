7baf4fa607dce8d1c5f0ac2edfd6bdec46827fa4
Sean Silvius <sean@Ditto.local>
2019-10-11 11:10:46 -0700

    Fixed typos, test passes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'populations', to: 'populations#index'
   get 'populations/by_year', to: 'populations#show'
-  get 'the_logz', to: 'logs#index'
+  get 'logs', to: 'logs#index'
 end
