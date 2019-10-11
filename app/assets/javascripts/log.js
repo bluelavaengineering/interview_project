4f76a62b382ac0c32cbc73948002235103ea639d
Sean Silvius <sean@Ditto.local>
2019-10-11 15:03:29 -0700

    Some turbolinks voodoo

--- app/assets/javascripts/log.js
+++ app/assets/javascripts/log.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready page:load', function() {
+    var REFRESH_INTERVAL_IN_MILLIS = 10000;
+     if ($('.f-pending-message').length > 0) {
+       setTimeout(function(){
+        Turbolinks.enableTransitionCache(true);
+        Turbolinks.visit(location.toString());
+        Turbolinks.enableTransitionCache(false);
+         }, REFRESH_INTERVAL_IN_MILLIS);
+    }
+});
