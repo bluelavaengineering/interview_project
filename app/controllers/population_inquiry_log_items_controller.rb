8e594115b3cc41df6192d208c5974f45146a7f77
Daniel Starling <ds@blinkbeam.com>
2019-11-24 17:21:24 -0800

    [CH-9] Refresh data every 10 seconds. (Skip tests for time)

--- app/controllers/population_inquiry_log_items_controller.rb
+++ app/controllers/population_inquiry_log_items_controller.rb
@@ -4,9 +4,13 @@ class PopulationInquiryLogItemsController < ApplicationController
   attr_reader :log_items, :year_to_query_count
   helper_method :log_items, :year_to_query_count
 
+  # NOTE: It would be more natural to implement a dashboard that replies to JSON and has a heavier
+  # JS front-end (e.g. at the very least, one with built-in templating so it can do its own JSON-to-UI
+  # transforms). We're taking advantage of template rendering server-side so we can keep the front-end
+  # dependency light, and so we don't have to bake a lacklustre front-end data binding framework.
   def index
     # REFACTOR: This needs pagination in the future
-    @log_items = PopulationInquiryLogItem.order('created_at desc').all
+    @log_items = PopulationInquiryLogItem.order(created_at: :desc).all
 
     # REFACTOR: This query could get expensive over time. You would want to index
     # the year fields the join occurs on, and consider caching this query response by the most recent
@@ -15,6 +19,8 @@ class PopulationInquiryLogItemsController < ApplicationController
                              .left_outer_joins(:population_inquiry_log_items)
                              .group(:year_number)
                              .order(:year_number)
-                             .pluck(:year_number, 'count(distinct population_inquiry_log_items.id)')
+                             .pluck(:year_number, Arel.sql('count(distinct population_inquiry_log_items.id)'))
+
+    render partial: 'tables', layout: false, locals: { log_items: log_items, year_to_query_count: year_to_query_count } if request.xhr?
   end
 end
